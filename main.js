bc4febec4d2e33d04fe164a9477ccd9e100885f5
Egor <e-okishev@mail.ru>
2023-07-10 20:47:21 +0300

    fix bug

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { numberOne, numberTwo, action, equally, answer, form, errorDiv, newDiv } from "./modules/const.js";
+import { numberOne, numberTwo, action, equally, answer, form, errorDiv } from "./modules/const.js";
 
 function calculate(numOne, action, numTwo) {
   if (action === 'plus') {
@@ -16,22 +16,20 @@ function calculate(numOne, action, numTwo) {
 }
 
 equally.addEventListener('click', function () {
+  const newDiv = document.createElement('div');
   if (numberOne.value.length > 0 && numberTwo.value.length > 0) {
     calculate(numberOne.value, action.value, numberTwo.value)
 
     errorDiv.remove()
+
     newDiv.classList.add('new_div')
     newDiv.textContent = answer.value;
     newDiv.style.cursor = 'pointer'
     form.appendChild(newDiv);
-
-    newDiv.addEventListener('click', function () {
-      newDiv.remove();
-    })
   } else {
-    errorDiv.classList.add('error_div');
-    errorDiv.style.color = 'red';
-    errorDiv.textContent = 'Заполните оба числа'
-    form.appendChild(errorDiv)
-  }
+      errorDiv.classList.add('error_div');
+      errorDiv.style.color = 'red';
+      errorDiv.textContent = 'Заполните оба числа'
+      form.appendChild(errorDiv)
+    }
 })
--- modules/const.js
+++ modules/const.js
@@ -3,6 +3,5 @@ export const numberTwo = document.getElementById('number_two');
 export const action = document.getElementById('action');
 export const equally = document.getElementById('equally');
 export const answer = document.getElementById('answer');
-export const form = document.querySelector('form')
+export const form = document.querySelector('form');
 export const errorDiv = document.createElement('div');
-export const newDiv = document.createElement('div');
